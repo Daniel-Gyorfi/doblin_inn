b3919a84386a7d8b23cc917bcb98440a91154107
Daniel Gyorfi <gyorfidaniel@tutanota.com>
2024-02-19 23:46:26 -0500

    working on tailwind, using base layout template for pages; neither is done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # doblin_inn
-Multi-purpose promotional website for the Doblin Inn company. Built with a variety of tools, such as Gin(Go), HTMX, Tailwind, Alpine.JS, Templ, Postgres
+Multi-purpose promotional website for the Doblin Inn company. Built with a variety of tools, including Gin(Go), HTMX, Tailwind, Alpine.JS, Templ, Postgres
--- templates/common_templ.go
+++ templates/common_templ.go
@@ -23,7 +23,7 @@ func Header() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav class=\"inline\"><ul><li><a href=\"\">DoblINN</a></li><li>Order</li><li>About</li></ul></nav>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav class=\"\"><ul class=\"inline-block list-none bg-red-200\"><li><a href=\"\">DoblINN</a></li><li>Order</li><li>About</li></ul></nav>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
