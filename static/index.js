2792f6fb7942f10f138b76b77c62bc825e16c824
Daniel Gyorfi <gyorfidaniel@tutanota.com>
2024-03-25 21:11:19 -0400

    adjusted navbar red borders; made index blurb size more mobile friendly maybe; index blurb has different shading, little more contrast; trimmed index text fade script; still some white space

--- static/index.js
+++ static/index.js
@@ -1,12 +1,8 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.debug("entry exists")
         if (entry.isIntersecting) {
-            console.debug("entry detected")
-            entry.target.classList.add("reveal")
-        } else {
-            console.debug()
-        }
+            entry.target.classList.add("reveal");
+        }  
     })
 });
 
